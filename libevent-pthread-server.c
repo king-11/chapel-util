c0122f639a9c76aa0fd10f24d9637bf6b7839473
Lakshya Singh <lakshay.singh1108@gmail.com>
2021-08-07 21:04:13 +0530

    feat: libevent server using pthread event loop
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

---
+++ libevent-pthread-server.c
@@ -0,0 +1,104 @@
+/*
+ * echo-server.c
+ *
+ * This is a modified version of the "simpler ROT13 server with
+ * Libevent" from:
+ * http://www.wangafu.net/~nickm/libevent-book/01_intro.html
+ */
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+
+#include <event2/event.h>
+#include <event2/thread.h>
+
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include <pthread.h>
+
+#define MAX_LINE 16384
+
+struct event_base *base;
+
+void
+on_accept(evutil_socket_t listener, short event, void *arg)
+{
+     struct event_base *base = arg;
+     struct sockaddr_storage ss;
+     socklen_t slen = sizeof(ss);
+     int fd = accept(listener, (struct sockaddr*)&ss, &slen);
+     if (fd < 0) {
+	  perror("accept");
+     } else if (fd > FD_SETSIZE) {
+	  close(fd);
+     } else {
+	  struct bufferevent *bev;
+	  evutil_make_socket_nonblocking(fd);
+	  event_base_loopbreak(base);
+     }
+}
+
+
+void* createBase() {
+     if (!base){
+	perror("base not created");
+     }
+     else {
+     	event_base_dispatch(base);
+     	event_base_free(base);
+     }
+
+     pthread_exit(0);
+}
+
+int
+main(int argc, char **argv)
+{
+     // Part 1
+     // where to initialize pthread and base
+     evthread_use_pthreads();
+     base = event_base_new();
+     pthread_t tid;
+     pthread_attr_t attr;
+     pthread_attr_init(&attr);
+     pthread_create(&tid, &attr, createBase, NULL);
+     
+     // Part 2
+     // stays same as earlier implementation
+     int listener;
+     struct sockaddr_in sin;
+     struct event *listener_event;
+
+     sin.sin_family = AF_INET;
+     sin.sin_addr.s_addr = 0;
+     sin.sin_port = htons(40713);
+
+     listener = socket(AF_INET, SOCK_STREAM, 0);
+     evutil_make_socket_nonblocking(listener);
+     if (bind(listener, (struct sockaddr*)&sin, sizeof(sin)) < 0) {
+	  perror("bind");
+	  return 1;
+     }
+
+     if (listen(listener, 16) < 0) {
+	  perror("listen");
+	  return 1;
+     }
+
+     listener_event = event_new(base, listener, EV_READ , on_accept, (void *)base);
+     event_add(listener_event, NULL);
+
+     // add waiting logic using qthreads here
+     // wake up by createBase thread
+     // how will the wake up work in case of multiple event observers
+
+     // Part 3
+     // where should this be added
+     pthread_join(tid, NULL);
+     return 0;
+}
